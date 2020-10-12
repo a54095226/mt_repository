560fcc6fff273b7013014eb941ba2b252629e262
yao <54095226@qq.com>
2020-10-12 21:57:09 +0800

    测试maven项目

---
+++ DubboWeb/src/main/java/cn/mt/xy/DubboWeb/WebApplication.java
@@ -0,0 +1,15 @@
+package cn.mt.xy.DubboWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class WebApplication {
+	 public static void main(String[] args) {
+         SpringApplication.run(WebApplication.class, args);
+     }
+}
