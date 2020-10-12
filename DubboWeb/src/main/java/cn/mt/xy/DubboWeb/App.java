37b541dd7f5125d24393d5a4adfbb382c0c2c0ea
yao <54095226@qq.com>
2020-10-12 21:41:28 +0800

    前端项目

---
+++ DubboWeb/src/main/java/cn/mt/xy/DubboWeb/App.java
@@ -0,0 +1,13 @@
+package cn.mt.xy.DubboWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
